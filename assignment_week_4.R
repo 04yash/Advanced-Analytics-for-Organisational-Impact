c87473bbbb87c3c2af43ed6f6b81d580037520fe
yashveer <103215836+04yash@users.noreply.github.com>
2023-01-15 22:12:35 +0530

    new changes

--- assignment_week_4.R
+++ assignment_week_4.R
@@ -14,7 +14,7 @@ turtle_sales
 
 # creating a new data frame from the subset of sales data frame.
 
-# Remove the '' and 'region' columns.
+# Remove the 'Ranking' and 'region' columns.
 turtle_sales <- select(turtle_sales, -Ranking, -Year, -Genre, -Publisher)
 
 # Check the new data frame.
@@ -28,34 +28,36 @@ summary(turtle_sales)
 # Specify X as EU_Sales, y as Global_Sales , and turtle_sales as the data source 
 # (the x-axis variable is passed first, followed by the y-axis,
 #  and then the source of the data is specified).
-qplot(EU_Sales, Global_Sales, data=turtle_sales)
+qplot(Global_Sales, Product, data=turtle_sales)
+qplot(NA_Sales, Product, data=turtle_sales)
+qplot(EU_Sales, Product, data=turtle_sales)
+
 qplot(Product, Platform, data=turtle_sales)
 
 # Create a scatterplot with a -variable.
 # Assign wage to the y variable, followed by the data source.
 qplot(y=EU_Sales, data=turtle_sales)
 qplot(y=Global_Sales, data=turtle_sales)
-
+qplot(y=Platform, data=turtle_sales)
+qplot(y=Product, data=turtle_sales)
+qplot(y=NA_Sales, data=turtle_sales)
 # Create a histogram
 # First pass the x-variable, then specify the data source. 
 qplot(Platform, data=turtle_sales)
 qplot(Platform, bins=5, data=turtle_sales)
 
+# Create a histogram
+# First pass the x-variable, then specify the data source. 
+qplot(Global_Sales, data=turtle_sales)
+qplot(Global_Sales, bins=5, data=turtle_sales)
+
 # Boxplot
-qplot(Product, Platform, data=turtle_sales, geom='boxplot')
+qplot(NA_Sales, EU_Sales, data=turtle_sales, geom='boxplot')
 
 
-# Any relationship between Platform, EU_Sales, and NA_Sales?
-# View the third plot.
-qplot(Product,
-      Platform,
-      colour=Global_Sales,
-      data=turtle_sales,
-      geom='boxplot')
-  
+
 
 # Observations and Insights from initial data.
-
 # we were able to eliminate various other columns which were non usable 
 # the initial outlook suggest that x360 sales were highest
 # the ps3 and pc games sales were in good proximity and were 2nd highest
